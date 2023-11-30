750e759b236046f31e8bd3848d875412d209f1f0
Fante <1115dave@gov>
2023-11-30 10:51:48 -0500

    toyotaby

---
+++ toyota.js
@@ -0,0 +1,11 @@
+// Function to calculate the area of a rectangle
+function calculateRectangleArea(length, width) {
+    return length * width;
+  }
+  
+  // Example usage of the function
+  let length = 5;
+  let width = 10;
+  let area = calculateRectangleArea(length, width);
+  console.log("The area of the rectangle is: " + area);
+  
